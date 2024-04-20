5ea13e7878db25a9f88711b9b9a58efc9ef70f15
jmvillafanamdoza <144737939+jmvillafanamdoza@users.noreply.github.com>
2024-04-20 13:20:18 -0500

    Primer commit subiendo proyecto

---
+++ Proyecto_Integrador_Prestamos/Context/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Integrador_Prestamos.Models;
+
+namespace Proyecto_Integrador_Prestamos.Context
+{
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext>options):base(options) 
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
